f61f784d20a3cd5a14eb4b03aaa78b85fd50024c
Tabitha Ricketts <trickett@nd.edu>
2016-12-27 19:19:20 -0500

    content upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function toRegistry(){
+    location.href= "https://www.myregistry.com/wedding-registry/Erin-Ricketts-Brian-Templet-New-Orleans-LA/1265682"
+}
+
+function toDetails(){
+    location.href="details.html"
+}
+
+function toRSVP(){
+    location.href="rsvp.html"
+}
